beeed0771001ecfdc56a6c9a19ddbfa90381ec27
TarlaGame <tarlagame@163.com>
2025-11-13 17:41:51 +0800

    feat: update path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // IMPORTANT: replace 'your-repo-name' with your actual GitHub repository name
 // so that the site assets are served from the correct subpath on GitHub Pages.
 export default defineConfig({
-  base: '/tarlah-game-web/',
+  base: '/',
   plugins: [react()]
 })
