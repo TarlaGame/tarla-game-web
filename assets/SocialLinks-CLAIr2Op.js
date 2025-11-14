8b022d5b016c5a5c2487acf11a72ce88bf4ace78
TarlahGame <tarlagame@163.com>
2025-11-14 02:24:31 +0000

    Deploying to gh-pages from @ TarlahGame/tarlah-game-web@75d5e7f9bea509a9a9e965f2cb794e6ffcd3f043 🚀

---
+++ assets/SocialLinks-CLAIr2Op.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CP4c5TDG.js";function i({links:r}){return e.jsx("div",{className:"social-list",children:r.map(a=>e.jsx("a",{className:"social-item",href:a.url,target:"_blank",rel:"noreferrer","aria-label":a.label,children:a.label},a.id))})}export{i as default};
